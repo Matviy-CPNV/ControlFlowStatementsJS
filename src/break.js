a8af768412a767a0602b6e00bbf713e15fe02145
px80gjy <matviy.lyubivyy1@eduvaud.ch>
2024-09-13 14:42:06 +0200

    feat: break on time

--- src/break.js
+++ src/break.js
@@ -1,8 +1,22 @@
 const numbers = [];
+let stoper = 0;
+let count = 0;
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
+    for (number of numbers) {
+        if (number > 50){
+            stoper += 1;
+        }
+    }
+    if (stoper === 1){
+        count = numbers.length;
+        count = 10 - count;
+        break;
+
+    }
 }
 
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
 }
+console.log(`Nombre d'opérations que votre amélioration a permis d'éviter à votre machine: ${count}`);
